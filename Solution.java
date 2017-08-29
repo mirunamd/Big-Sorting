3952cd3dc204ed7eccdfb6132a81452d1c09761f
Miruna Dumitrascu <miru_dumi@yahoo.com>
2017-08-29 14:46:24 +0300

    Used basic sort

---
+++ Solution.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    private static BigInteger[] a;
+        
+    private static void sort(int n, BigInteger[] a){
+        for(int i = 0; i < n - 1; i++)
+            for(int j = i + 1; j < n; j++)
+                if(a[i].compareTo(a[j]) == 1){
+                    BigInteger bi = a[i];
+                    a[i] = a[j];
+                    a[j] = bi;                    
+                }
+        for(int i = 0; i < n; i++)
+            System.out.println(a[i]);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        a = new BigInteger[n];
+        for(int unsorted_i=0; unsorted_i < n; unsorted_i ++){
+            String s = in.next();
+            a[unsorted_i] = new BigInteger(s);
+        }
+        
+        sort(n, a);
+    }
+}
